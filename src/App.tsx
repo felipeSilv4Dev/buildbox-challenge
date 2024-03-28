867588fc26f19b03e975d732182ea6c27d94bc70
felipeSilv4Dev <felipesilva.developer@gmail.com>
2024-03-28 16:05:42 -0300

    created the header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
